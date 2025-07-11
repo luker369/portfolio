9f2bbdbb63f7ab7be90b4b72edd7197cdc487f29
luker369 <walker0369@gmail.com>
2025-07-11 10:51:09 -0500

    initial commit

---
+++ src/constants/constants.js
@@ -0,0 +1,80 @@
+export const projects = [
+  [
+    {
+      title: 'Bank',
+      description: "description",
+      image: './images/3.jpg',
+      
+      visit: 'https://hoobank-six-theta.vercel.app/',
+    },
+    {
+      title: 'Restaurant',
+      description: "description",
+      image: './images/5.jpg',
+      
+      visit: 'https://restaurant-psi-rose.vercel.app/',
+    },
+    {
+      title: 'GPT',
+      description: "description",
+      image: './images/4.png',
+      
+      visit: 'https://gpt-theme-project-bj4f.vercel.app/',
+    }
+  ]
+];
+
+
+export const TechnologiesImgs = [
+  {
+    image: '/images/html.png',
+    name: "html",
+  },
+
+  {
+    image: '/images/css.png',
+    name: "css",
+  },
+
+  {
+    image: '/images/javascript.png',
+    name: "javascript",
+  },
+
+  {
+    image: '/images/bootstrap5.png',
+    name: "bootstrap",
+  },
+
+  {
+    image: '/images/Tailwind-CSS.png',
+    name: "tailwind css",
+  },
+
+  {
+    image: '/images/react.png',
+    name: "react js",
+  },
+
+  {
+    image: '/images/redux.png',
+    name: "redux",
+  },
+
+  {
+    image: '/images/git.png',
+    name: "git",
+  },
+
+  {
+    image: '/images/github.png',
+    name: "github",
+  },
+]
+
+
+export const testimonials = [
+  // {
+  //   image: ""
+  // },
+]
