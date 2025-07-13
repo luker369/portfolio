a150dc37814bac759fa78dccd87219f95a820c17
luker369 <walker0369@gmail.com>
2025-07-12 22:36:12 -0500

    installed my own media links and added descriptions to the projects array

--- src/componenets/SocialLinks.jsx
+++ src/componenets/SocialLinks.jsx
@@ -1,17 +1,17 @@
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillFacebook, AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 
 
 const SocialLinks = () => {
   return (
     <div className="social-links flex gap-[10px]">
     <a className='w-[40px] h-[40px] rounded-full text-[28px]
-    flex justify-center items-center duration-[0.4s] hover:bg-[#212d45] hover:scale-[1.2]' href="https://github.com/mehdihou10" target='__blank'><AiFillGithub /></a>
+    flex justify-center items-center duration-[0.4s] hover:bg-[#212d45] hover:scale-[1.2]' href="https://github.com/luker369" target='__blank'><AiFillGithub /></a>
     
     <a className='w-[40px] h-[40px] rounded-full text-[28px]
-    flex justify-center items-center duration-[0.4s] hover:bg-[#212d45] hover:scale-[1.2]' href="https://www.linkedin.com/in/mehdi-houari-001115256/" target='__blank'><AiFillLinkedin /></a>
+    flex justify-center items-center duration-[0.4s] hover:bg-[#212d45] hover:scale-[1.2]' href="https://www.linkedin.com/in/luke-sterling-a191b0247/" target='__blank'><AiFillLinkedin /></a>
     
     <a className='w-[40px] h-[40px] rounded-full text-[28px]
-    flex justify-center items-center duration-[0.4s] hover:bg-[#212d45] hover:scale-[1.2]' href="https://www.instagram.com/mehdi_hou_10/?igshid=ZDdkNTZiNTM%3D" target='__blank'><AiFillInstagram /></a>
+    flex justify-center items-center duration-[0.4s] hover:bg-[#212d45] hover:scale-[1.2]' href="https://www.facebook.com/luke.sterling.527436" target='__blank'><AiFillFacebook /></a>
 
   </div>
   )
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,21 +2,21 @@ export const projects = [
   [
     {
       title: 'Bank',
-      description: "description",
+      description: "At the Next Generation Bank we are redefining the future of banking. Our cutting-edge website offers a seamless and innovative banking experience",
       image: './images/3.jpg',
       
       visit: 'https://hoobank-six-theta.vercel.app/',
     },
     {
       title: 'Restaurant',
-      description: "description",
+      description: "A modern landing page showcasing our exquisite menu, ambiance, and easy reservations. Discover culinary excellence where every dish is a masterpiece.",
       image: './images/5.jpg',
       
       visit: 'https://restaurant-psi-rose.vercel.app/',
     },
     {
       title: 'GPT',
-      description: "description",
+      description: "Prepare to be amazed by the next evolution in artificial intelligence. GPT-4 Alpha One is here to redefine the boundaries of what's possible in the world of AI and natural language processing.",
       image: './images/4.png',
       
       visit: 'https://gpt-theme-project-bj4f.vercel.app/',
